c858cceb4c481195df95ca85bb765a441339892b
ArticSaber <kaarthiknirmala@gmail.com>
2023-09-06 20:00:10 +0530

    added products

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 import express from "express";
-const app = express();
-import shoppingCartRouter from "./routes/shoppingCartRoute.js";
-import authRoute from "./routes/authRoute.js";
-import mongoose from "mongoose";
-import {} from "dotenv/config.js";
-import { v2 as cloudinary } from "cloudinary";
+import dbConnection from "./db/dbConnection.js";
+import dotenv from "dotenv";
 import cors from "cors";
+import authRoute from "./routes/authRoute.js";
+import productRoute from "./routes/productRoute.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+
+dotenv.config();
+
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://amazon-kaarthik.vercel.app/"],
   credentials: true,
 };
+
+const PORT = process.env.PORT || 3500;
+const app = express();
+
 app.use(cors(corsOptions));
 app.use(express.json());
-const PORT = 3500;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
-
-mongoose.connect(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", (errorMessage) => console.log(errorMessage));
-db.once("open", () => console.log(`Connected successfully to database`));
-
-app.use("/api/v1/shoppingcart", shoppingCartRouter);
+app.use(cookieParser());
+app.use(fileUpload({ useTempFiles: true }));
+app.use("/api/v1/products", productRoute);
 app.use("/api/v1/auth", authRoute);
 
-app.listen(
-  PORT,
-  console.log(
-    `Server started running at http://localhost:${PORT}/api/v1/shoppingcart/`
-  )
-);
 
-export default app;
+const connection = () => {
+  try {
+    dbConnection(process.env.DB_URL);
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error in connecting to DB", error);
+  }
+};
+
+connection();
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,55 +1,14 @@
-import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import Express from "express";
+import {
+  authSignup,
+  authlogin,
+  authpasswordreset,
+} from "../controllers/authController.js";
 
-const router = express.Router();
+const authrouter = Express.Router();
 
-router.post("/signup", async (req, res) => {
-  try {
-    const { email, password, username } = req.body;
-    console.log(email, password, username);
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already registered" });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    await newUser.save();
-    res.status(201).json({ message: "User registered successfully" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+authrouter.route("/signup").post(authSignup);
+authrouter.route("/login").post(authlogin);
+authrouter.route("/password-reset").patch(authpasswordreset);
 
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    console.log(email, password);
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-    const token = jwt.sign({ userId: user._id }, "secretKey", {
-      expiresIn: "1h",
-    });
-    res.cookie("token", token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24,
-      domain: "localhost",
-      path: "/",
-    });
-    console.log(token, isPasswordValid);
-    res.status(200).json({ userId: user._id, userName: user.email });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-});
-
-export default router;
+export default authrouter;
